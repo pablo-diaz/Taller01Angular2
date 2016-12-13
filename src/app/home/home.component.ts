c4d5b371b9153453f281615ee29ea0d36bd95080
Pablo Andres Diaz <pabloandresd@gmail.com>
2016-12-12 19:29:56 -0500

    Added more layout beauty

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<input type="image" src="/assets/imgs/eLearning.jpg" alt="eLearning image">`
+  template: `<img src="/assets/imgs/eLearning.jpg" alt="eLearning image" class="img-thumbnail">`
 })
 export class HomeComponent implements OnInit {
 
